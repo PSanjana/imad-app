f6d1c298720158cd044ce099c883179d264808e5
PSanjana <30874730+PSanjana@users.noreply.github.com>
2017-08-27 18:37:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/', function (req, res) {
 
 app.get('/article-one', function (req, res)
 {
-   res.Send(createtemplate(articleone));
+   res.send(createtemplate(articleone));
 });
 
 app.get('/article-two', function (req, res)
