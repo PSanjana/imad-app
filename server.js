01b3d161baf01d7bbd6d3c67bdd57069f72aa315
PSanjana <30874730+PSanjana@users.noreply.github.com>
2017-09-06 15:50:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,21 +102,37 @@ app.get('/test-db', function(req,res){
         else{
             res.send(JSON.stringify(result));
         }
-    })
-})
-
+    });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var pool = new Pool(config);
+app.get('/test-db', function(req,res){
+    //make a select request
+    //return a response to the request
+    pool.query('SELECT * from test' , function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result));
+        }
+    });
+});
 var counter = 0;
 app.get('/counter', function(req, res) {
     counter=counter + 1;
     res.send(counter.toString());
 });
-
 var names=[];
 app.get('/submit_name', function(req, res){//URL: submit_name?name=xxxxx
     var name= req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
+
+    });
 });
+
 
 app.get('/:articlename', function (req, res)
 {
