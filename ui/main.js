c87370961741b92c9a1f087486f3a39048408371
PSanjana <30874730+PSanjana@users.noreply.github.com>
2017-09-06 08:39:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,17 +22,34 @@ button.onclick = function () {
    
 } ;
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     
-    //capture a list of names and render it as a list
-    var names =['name1','name2','name3','name4'];
-    var list='';
-    for( var i = 0; i< names.length; i++){
-        list += '<li>'+ names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    //make a request to the server and send the name
+    
+      //capture the reponse and store it in a variable
+    request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                //capture a list of names and render it as a list
+                var names =request.responseText;
+                names=JSON.parse(names);
+                var list='';
+                for( var i = 0; i< names.length; i++){
+                    list += '<li>'+ names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+    };
+    
+    //make a request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET', 'http://psanjuupadhyaya.imad.hasura-app.io/submit_name?name=',true);
+    request.send(null);
+    
+   
 };
